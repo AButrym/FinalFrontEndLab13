1aedb9017c9555c3cdcc5e0704bac35252f29b4b
AButrym <butrym@karazin.ua>
2020-06-10 10:49:57 +0300

    product details page styles finished

--- OleksandrButrym/js/product-details.js
+++ OleksandrButrym/js/product-details.js
@@ -1,1 +1,11 @@
 // add products to purchase list are kept in local storage
+const productPicture = document.querySelector('.details__picture');
+productPicture.style.backgroundImage = 'url("img/details/01.jpg")';
+const productMiniatures = document.querySelectorAll('.details__picture > div');
+let ixPicture = 2;
+for (const el of productMiniatures) {
+  el.style.backgroundImage = `url("img/details/0${ixPicture++}.jpg")`;
+  el.addEventListener('click', (e) => {
+    [el.style.backgroundImage, productPicture.style.backgroundImage] = [productPicture.style.backgroundImage, el.style.backgroundImage];
+  });
+}
